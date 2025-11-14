dec1a475f44d9cc3557b2b836b41827d971f4257
P　R　O　F　E　S　O　R <120622253+DEVIL-DOOR@users.noreply.github.com>
2025-11-14 11:51:09 +0600

    README.md

--- README.py
+++ README.py
@@ -1,9 +1,2 @@
 # iNFiNiTY-WSATSAPP-REPORT
 গুরুত্বপূর্ণ নোট শুধুমাত্র শিক্ষামূলক উদ্দেশ্যে অপব্যবহার করবেন না WhatsApp-এর নীতি লঙ্ঘন করলে আপনার দায়িত্ব
-ডিপেন্ডেন্সি ইনস্টল
-ডিপেন্ডেন্সি ইনস্টল
-pip install requests rich
-ইনস্টল কমান্ড
-git clone https://github.com/DEVIL-DOOR/iNFiNiTY-WSATSAPP-REPORT.git
- রান করুন
-python infinity_report.py
